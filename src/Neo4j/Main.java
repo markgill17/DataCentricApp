632b66a21d97cbf39a08a97e322815a256cebea3
markgill17 <G00348787@gmit.ie>
2018-12-12 23:53:35 +0000

    Add files via upload

--- src/Neo4j/Main.java
+++ src/Neo4j/Main.java
@@ -1,10 +1,26 @@
 package Neo4j;
 
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.Transaction;
+import org.neo4j.driver.v1.TransactionWork;
+import static org.neo4j.driver.v1.Values.parameters;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void createNodes(String name, String address) {
+		Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "neo4jdb"));
+		Session session = driver.session();
+		session.writeTransaction(new TransactionWork<String>() {
+			@Override
+			public String execute(Transaction tx) {
+				tx.run("CREATE (:Student {name:{name}})", parameters("name", name));
+				tx.run("CREATE (:Address {name:{address}})", parameters("address", address));
+				return null;
+			}
+		});
 
 	}
-
 }
